c13ef728337622c9fe5ece15cb680cdd9e789789
Barsch2006 <christian0511@gmx.de>
2024-05-18 21:24:49 +0200

    no more agenda types

--- frontend/src/content/config.ts
+++ frontend/src/content/config.ts
@@ -4,19 +4,6 @@ const agendaPointSchema = z.object({
     date: z.string().regex(/^\d{2}.\d{2}.\d{4}$/), // DD.MM.YYYY
     time: z.string().regex(/^\d{2}:\d{2}$/), // HH:MM
     title: z.string(), // name of the agenda point
-    /*
-     * start - the beginning of the event
-     * default - the default point of the agenda
-     * pause - the pause between two points
-     * q-and-a - the question and answer session with the audience
-     * speech - a speech or presentation
-     * special - any special performance during the event
-     * end - the end of the event
-     */
-    type: z
-        .enum(["start", "default", "pause", "q-and-a", "performance", "speech", "end"])
-        .optional()
-        .default("default"),
 });
 
 const agendaCollection = defineCollection({
