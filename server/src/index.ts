c7aef10ce1601edd1ce1426fa017cd34e2cb2253
Barsch2006 <christian0511@gmx.de>
2024-05-18 17:37:01 +0200

    addd socket

---
+++ server/src/index.ts
@@ -0,0 +1,18 @@
+import { Server } from "socket.io";
+import http from "node:http";
+import express from "express";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+    console.log("a user connected");
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+    });
+});
+
+server.listen(8080, () => {
+    console.log("listening on localhost:8080");
+});
