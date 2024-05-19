7fa90a675e48386e380d5a576ed7315bc5f51ca1
Barsch2006 <christian0511@gmx.de>
2024-05-19 12:24:48 +0200

    fix beamer view, ControlBtn for Socket

---
+++ frontend/src/components/ControlBtn.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import socket from "../services/Socket";
+
+export default {
+    props: {
+        label: {
+            type: String,
+            required: true,
+        },
+        wstarget: {
+            type: String,
+            required: true,
+        },
+    },
+    methods: {
+        sendAction() {
+            socket.emit(this.wstarget);
+        },
+    },
+};
+</script>
+
+<template>
+    <button
+        @click="sendAction"
+        class="font-semi-bold rounded border-2 border-transparent bg-neutral-800 px-3 py-1 text-lg transition-colors duration-300 ease-out hover:border-neutral-300"
+    >
+        {{ label }}
+    </button>
+</template>
