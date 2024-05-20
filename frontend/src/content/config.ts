0f19c826d559bee40f765c4a4d699b53f651c19f
Christian Fuchte <109520303+chfuchte@users.noreply.github.com>
2024-05-20 14:31:16 +0200

    Update config.ts

--- frontend/src/content/config.ts
+++ frontend/src/content/config.ts
@@ -2,7 +2,7 @@ import { z, defineCollection } from "astro:content";
 
 const agendaPointSchema = z.object({
     date: z.string().regex(/^\d{2}.\d{2}.\d{4}$/), // DD.MM.YYYY
-    time: z.string().regex(/^\d{2}:\d{2}$/), // HH:MM
+    time: z.string().regex(/^(?:[01]\d|2[0-3]):[0-5]\d(?: - (?:[01]\d|2[0-3]):[0-5]\d)?$/), // HH:MM or HH:MM - HH:MM
     title: z.string(), // name of the agenda point
 });
 
